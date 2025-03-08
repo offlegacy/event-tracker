a26c3e8f5bae06c84f60e1db7935d5591b519dbb
Hwang Taehwan <107744534+stakbucks@users.noreply.github.com>
2025-03-08 15:32:12 +0900

    docs: add Korean documentation + open graph metadata (#18)
    * docs: enable ko docs
    
    * docs: ko translate
    
    * fix: move not-found inside /docs
    
    * chore: og metadata

---
+++ docs/src/app/[lang]/layout.tsx
@@ -0,0 +1,85 @@
+import { Head } from "nextra/components";
+import { getPageMap } from "nextra/page-map";
+import { Footer, Layout, LocaleSwitch, Navbar } from "nextra-theme-docs";
+
+import "nextra-theme-docs/style-prefixed.css";
+import "../globals.css";
+import { TrackClick, TrackImpression, TrackProvider } from "@/tracker";
+import { Logo } from "@/logo";
+import { Lang } from "@/lib/types/lang";
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | event-tracker",
+    default: "event-tracker",
+  },
+  description:
+    "A lightweight, type-safe event tracking library for React applications that simplifies analytics integration while maintaining clean code and optimal performance.",
+  openGraph: {
+    title: {
+      template: "%s | event-tracker",
+      default: "event-tracker",
+    },
+    images: ["/og-webp.webp"],
+    description:
+      "A lightweight, type-safe event tracking library for React applications that simplifies analytics integration while maintaining clean code and optimal performance.",
+  },
+};
+
+const navbar = (
+  <Navbar
+    logo={
+      <TrackClick params={{ target: "logo" }}>
+        <div className="flex items-center gap-2">
+          <Logo />
+          <b>event-tracker</b>
+        </div>
+      </TrackClick>
+    }
+    projectLink="https://github.com/offlegacy/event-tracker"
+  >
+    <LocaleSwitch />
+  </Navbar>
+);
+const footer = (
+  <TrackImpression params={{ target: "footer" }}>
+    <Footer>MIT {new Date().getFullYear()} © OffLegacy</Footer>
+  </TrackImpression>
+);
+
+export default async function RootLayout({
+  params,
+  children,
+}: {
+  children: React.ReactNode;
+  params: Promise<{ lang: Lang }>;
+}) {
+  const { lang } = await params;
+  const pageMap = await getPageMap(lang);
+
+  return (
+    <html lang={lang} dir="ltr" suppressHydrationWarning>
+      <Head>
+        <link rel="icon" href="/logo.jpg" type="image/jpg" />
+      </Head>
+      <body>
+        <TrackProvider initialContext={{ referrer: typeof window !== "undefined" ? document.referrer : "" }}>
+          <Layout
+            i18n={[
+              { locale: "en", name: "English" },
+              { locale: "ko", name: "한국어" },
+            ]}
+            navbar={navbar}
+            pageMap={pageMap}
+            docsRepositoryBase="https://github.com/offlegacy/event-tracker/tree/main/docs"
+            footer={footer}
+            darkMode
+          >
+            {children}
+          </Layout>
+        </TrackProvider>
+      </body>
+    </html>
+  );
+}
---
+++ docs/src/content/ko/components/_meta.ts
@@ -0,0 +1,48 @@
+import { MetaRecord } from "nextra";
+
+const meta: MetaRecord = {
+  index: {
+    title: "Provider",
+    theme: {
+      toc: true,
+      layout: "default",
+    },
+  },
+  "dom-event": {
+    title: "DOMEvent",
+    theme: {
+      toc: true,
+      layout: "default",
+    },
+  },
+  click: {
+    title: "Click",
+    theme: {
+      toc: true,
+      layout: "default",
+    },
+  },
+  impression: {
+    title: "Impression",
+    theme: {
+      toc: true,
+      layout: "default",
+    },
+  },
+  "page-view": {
+    title: "PageView",
+    theme: {
+      toc: true,
+      layout: "default",
+    },
+  },
+  "set-context": {
+    title: "SetContext",
+    theme: {
+      toc: true,
+      layout: "default",
+    },
+  },
+};
+
+export default meta;
